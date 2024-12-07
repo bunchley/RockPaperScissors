a01f58845d4bcc059051719fbf8a2fb39f695fca
Ashley Bunch <bunchley007@gmail.com>
2024-12-07 10:07:13 -0700

    fix bug

--- script/game.js
+++ script/game.js
@@ -1,14 +1,13 @@
-
 const rock_weapon = document.getElementById("rock");
 const paper_weapon = document.getElementById("paper");
 const scissors_weapon = document.getElementById("scissors");
 const scoreboard_computer = document.getElementById("computerScore");
 const scoreboard_player = document.getElementById("playerScore");
 const result = document.querySelector(".result");
-const gameEnd = document.querySelector(".game-over")
-let computerScore =0;
-let playerScore=0;
-let whoWon; 
+const gameEnd = document.querySelector(".game-over");
+let computerScore = 0;
+let playerScore = 0;
+let whoWon;
 
 //modal
 var modal = document.querySelector(".modal");
@@ -16,86 +15,80 @@ var trigger = document.querySelector(".trigger-help");
 var closeButton = document.querySelector(".close-button");
 
 function toggleModal() {
-    modal.classList.toggle("show-modal");
+  modal.classList.toggle("show-modal");
 }
 
 function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
+  if (event.target === modal) {
+    toggleModal();
+  }
 }
 
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
-function computerPlay() {
-    const computerWeapon=['Rock', 'Paper', 'Scissors'];
-    let randomChoice=Math.floor(Math.random()*3);
-    return computerWeapon[randomChoice];
+function getComputerChoice() {
+  const computerWeapon = ["Rock", "Paper", "Scissors"];
+  let randomChoice = Math.floor(Math.random() * 3);
+  return computerWeapon[randomChoice];
 }
 
-function playRound ( playerChoice){
-    computerChoice = computerPlay();
-    console.log('Player Choice: ', playerChoice);
-    console.log('Computer Choice: ',computerChoice);
-    if (computerChoice === playerChoice){
-        console.log("It's a tie!");
-        result.innerHTML = `It's a ${computerChoice} tie!`;
-        whoWon="noone";
-    }
-    else if (computerChoice === "Paper" && playerChoice ==="Rock" || computerChoice === "Rock" && playerChoice ==="Scissors" || computerChoice === "Scissors" && playerChoice==="Paper"){
-        console.log("Computer wins this round");
-        result.innerHTML = `Bot's ${computerChoice} beats your ${playerChoice}!`;
-        whoWon="computer";
-    }
-    else {
-        console.log("You won!");
-        result.innerHTML = `Your ${playerChoice} beats Bot's ${computerChoice}!`;
-        whoWon="player";
-    }
-    updateScoreboard(whoWon);
-
-
+function playRound(playerChoice, computerChoice = getComputerChoice()) {
+  //   computerChoice = getComputerChoice();
+  console.log("Player Choice: ", playerChoice);
+  console.log("Computer Choice: ", computerChoice);
+  if (computerChoice === playerChoice) {
+    console.log("It's a tie!");
+    result.innerHTML = `It's a ${computerChoice} tie!`;
+    whoWon = "noone";
+  } else if (
+    (computerChoice === "Paper" && playerChoice === "Rock") ||
+    (computerChoice === "Rock" && playerChoice === "Scissors") ||
+    (computerChoice === "Scissors" && playerChoice === "Paper")
+  ) {
+    console.log("Computer wins this round");
+    result.innerHTML = `Bot's ${computerChoice} beats your ${playerChoice}!`;
+    whoWon = "computer";
+  } else {
+    console.log("You won!");
+    result.innerHTML = `Your ${playerChoice} beats Bot's ${computerChoice}!`;
+    whoWon = "player";
+  }
+  updateScoreboard(whoWon);
 }
 
-function updateScoreboard(whoWon){
-    if (whoWon === "player"){
-        playerScore++;
-    }
-    else if (whoWon ==="computer"){
-        computerScore++;
-    }
-    scoreboard_computer.innerHTML = computerScore;
-    scoreboard_player.innerHTML = playerScore;
+function updateScoreboard(whoWon) {
+  if (whoWon === "player") {
+    playerScore++;
+  } else if (whoWon === "computer") {
+    computerScore++;
+  }
+  scoreboard_computer.innerHTML = computerScore;
+  scoreboard_player.innerHTML = playerScore;
+  isGameOver(computerScore, playerScore);
 }
-function isGameOver(computer, player){
-    if(player === 5){
-        gameEnd.innerHTML= 'You Win!!!'
-        gameOver=true;
-
-    }
-    else if(computer===5){
-        gameEnd.innerHTML= 'You Lose!!!'
-        gameOver=true;
-    }
-    return gameOver;
+function isGameOver(computer, player) {
+  if (player === 5) {
+    gameEnd.innerHTML = "You Win!!!";
+    gameOver = true;
+  } else if (computer === 5) {
+    gameEnd.innerHTML = "You Lose!!!";
+    gameOver = true;
+  }
+  return gameOver;
 }
 
-function game(){
-    computer_weapon = computerPlay();
-    rock_weapon.addEventListener('click', function() {
-        playRound( "Rock");
-    });
-    paper_weapon.addEventListener('click', function (){
-        playRound( "Paper");
-    });
-    scissors_weapon.addEventListener('click', function (){
-        playRound("Scissors");
-    });
-
-
+function game() {
+  computer_weapon = getComputerChoice();
+  rock_weapon.addEventListener("click", function () {
+    playRound("Rock");
+  });
+  paper_weapon.addEventListener("click", function () {
+    playRound("Paper");
+  });
+  scissors_weapon.addEventListener("click", function () {
+    playRound("Scissors");
+  });
 }
 game();
-
-
--- script/game_updated.js
+++ script/game_updated.js
@@ -1,10 +1,9 @@
-
 const scoreboard_computer = document.getElementById("computerScore");
 const scoreboard_player = document.getElementById("playerScore");
 const result = document.querySelector(".result");
-let computerScore =0;
-let playerScore=0;
-let whoWon; 
+let computerScore = 0;
+let playerScore = 0;
+let whoWon;
 
 //modal
 var modal = document.querySelector(".modal");
@@ -12,100 +11,100 @@ var trigger = document.querySelector(".trigger-help");
 var closeButton = document.querySelector(".close-button");
 
 function toggleModal() {
-    modal.classList.toggle("show-modal");
+  modal.classList.toggle("show-modal");
 }
 
 function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
+  if (event.target === modal) {
+    toggleModal();
+  }
 }
 
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
-
 function computerPlay() {
-    const computerWeapon=['Rock', 'Paper', 'Scissors'];
-    let randomChoice=Math.floor(Math.random()*3);
-    return computerWeapon[randomChoice];
+  const computerWeapon = ["Rock", "Paper", "Scissors"];
+  let randomChoice = Math.floor(Math.random() * 3);
+  return computerWeapon[randomChoice];
 }
 
-function playRound ( playerChoiceLowerCase){
-    computerChoice = computerPlay();
-    let playerChoice = playerChoiceLowerCase.charAt(0).toUpperCase() + playerChoiceLowerCase.substring(1);
-    if (computerChoice === playerChoice){
-        result.innerHTML = `It's a ${computerChoice} tie!`;
-        whoWon="noone";
-    }
-    else if (computerChoice === "Paper" && playerChoice ==="Rock" 
-        || computerChoice === "Rock" && playerChoice ==="Scissors" 
-        || computerChoice === "Scissors" && playerChoice==="Paper"){
-
-        result.innerHTML = `Bot's ${computerChoice} beats your ${playerChoice}!`;
-        whoWon="Bot";
-    }
-    else {
-        result.innerHTML = `Your ${playerChoice} beats Bot's ${computerChoice}!`;
-        whoWon="You";
-    }
-    return whoWon;
+function playRound(playerChoiceLowerCase) {
+  computerChoice = computerPlay();
+  let playerChoice =
+    playerChoiceLowerCase.charAt(0).toUpperCase() +
+    playerChoiceLowerCase.substring(1);
+  if (computerChoice === playerChoice) {
+    result.innerHTML = `It's a ${computerChoice} tie!`;
+    whoWon = "noone";
+  } else if (
+    (computerChoice === "Paper" && playerChoice === "Rock") ||
+    (computerChoice === "Rock" && playerChoice === "Scissors") ||
+    (computerChoice === "Scissors" && playerChoice === "Paper")
+  ) {
+    result.innerHTML = `Bot's ${computerChoice} beats your ${playerChoice}!`;
+    whoWon = "Bot";
+  } else {
+    result.innerHTML = `Your ${playerChoice} beats Bot's ${computerChoice}!`;
+    whoWon = "You";
+  }
+  return whoWon;
 }
 
-function updateScoreboard(whoWon){
-    if (whoWon === "You"){
-        playerScore++;
-    }
-    else if (whoWon ==="Bot"){
-        computerScore++;
-    }
-    scoreboard_computer.innerHTML = computerScore;
-    scoreboard_player.innerHTML = playerScore;
+function updateScoreboard(whoWon) {
+  if (whoWon === "You") {
+    playerScore++;
+  } else if (whoWon === "Bot") {
+    computerScore++;
+  }
+  scoreboard_computer.innerHTML = computerScore;
+  scoreboard_player.innerHTML = playerScore;
 }
 
-function isGameOver(){
-    let gameOver = false;
-    let computerScoreHTML = scoreboard_computer.innerHTML;
-    let playerScoreHTML = scoreboard_player.innerHTML;
-    if(computerScoreHTML === '5'){
-        gameOver = true;
-    }
-    else if (playerScoreHTML === '5'){
-        gameOver = true;
-    }
-    return gameOver;
+function isGameOver() {
+  let gameOver = false;
+  let computerScoreHTML = scoreboard_computer.innerHTML;
+  let playerScoreHTML = scoreboard_player.innerHTML;
+  if (computerScoreHTML === "5") {
+    gameOver = true;
+  } else if (playerScoreHTML === "5") {
+    gameOver = true;
+  }
+  return gameOver;
 }
 
-function game(){
-    window.addEventListener('click', function(e) {
+function game() {
+  window.addEventListener("click", function (e) {
+    // this.console.log(e.path [1].id);
+    const buttonSelected = document.querySelector(
+      `.weapon[data-key="${e.path[1].id}"]`
+    );
+    const audio = document.querySelector(`audio[data-key="${e.path[1].id}"]`);
+    const resultDisplay = document.querySelector(".result");
+    resultDisplay.classList.remove("gameOver");
 
-        this.console.log(e.path [1].id);
-        const buttonSelected = document.querySelector(`.weapon[data-key="${e.path [1].id}"]`);
-        const audio = document.querySelector(`audio[data-key="${e.path [1].id}"]`);
-        const resultDisplay = document.querySelector('.result');
-        resultDisplay.classList.remove('gameOver');
+    if (!audio) return; // dont play if user doesn't select button
+    audio.play();
+    buttonSelected.classList.add("playing");
 
-        if(!audio) return; // dont play if user doesn't select button 
-        audio.play();
-        buttonSelected.classList.add('playing');
-        
-        let winner = playRound(e.path [1].id);
-        updateScoreboard(winner);
-        if(isGameOver()){
-            resultDisplay.classList.add('gameOver');
-            result.innerHTML=`${winner} won! Select a button to clear the score and start a new game!`;
-            playerScore =0;
-            computerScore =0;
-        };
-
-    });
-    function removeTransition(e){
-        if(e.propertyName !== 'transform') return;
-        this.classList.remove('playing')
+    let winner = playRound(e.path[1].id);
+    updateScoreboard(winner);
+    if (isGameOver()) {
+      resultDisplay.classList.add("gameOver");
+      result.innerHTML = `${winner} won! Select a button to clear the score and start a new game!`;
+      playerScore = 0;
+      computerScore = 0;
     }
-    const weapons = document.querySelectorAll('.weapon');
-    weapons.forEach(weapon => weapon.addEventListener('transitionend', removeTransition));
-};
+  });
+  function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("playing");
+  }
+  const weapons = document.querySelectorAll(".weapon");
+  weapons.forEach((weapon) =>
+    weapon.addEventListener("transitionend", removeTransition)
+  );
+}
 
 game();
